df88e0c880cd825ed9ba4826e4f5cea386ba029e
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-05-06 21:39:41 +0530

    initial commit

---
+++ JsonData.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    id: 1,
+    prjname: "expense management system",
+    Image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0cE-nY5D8luW6z2KZlLdKj9tKofZta-Lt7A&usqp=CAU",
+    prjlink: "https://tiny-fedora-moth.cyclic.app",
+  },
+  {
+    id: 2,
+    prjname: "blog app",
+    Image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuhGYA23azG3NpvOEIUgvA1KQuHmZ7BZTF4Q&usqp=CAU",
+    prjlink: "https://blog-1hj5.onrender.com",
+  },
+];
+module.exports = data;
