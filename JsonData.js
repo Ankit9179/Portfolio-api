2da3f8ae6739cf9f711405745a930e6cef68c32a
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-05-07 07:16:38 +0530

    shanges in jsonData pfjname

--- JsonData.js
+++ JsonData.js
@@ -1,14 +1,14 @@
 const data = [
   {
     id: 1,
-    prjname: "expense management system",
+    prjname: "ems mern app",
     Image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0cE-nY5D8luW6z2KZlLdKj9tKofZta-Lt7A&usqp=CAU",
     prjlink: "https://tiny-fedora-moth.cyclic.app",
   },
   {
     id: 2,
-    prjname: "blog app",
+    prjname: "blog mern app",
     Image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuhGYA23azG3NpvOEIUgvA1KQuHmZ7BZTF4Q&usqp=CAU",
     prjlink: "https://blog-1hj5.onrender.com",
